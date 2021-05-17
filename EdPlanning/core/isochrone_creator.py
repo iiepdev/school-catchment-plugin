740a8e7945caaed2c81cfcfe1cbe5340ce92151b
Riku Oja <riku.oja@kapsi.fi>
2021-05-17 15:43:26 +0300

    First implementation of isochrone fetch

--- EdPlanning/core/isochrone_creator.py
+++ EdPlanning/core/isochrone_creator.py
@@ -1,9 +1,23 @@
+import json
+import logging
 from dataclasses import dataclass
 from typing import Optional
 
-from qgis.core import QgsVectorLayer
+from qgis.core import (
+    QgsFeature,
+    QgsGeometry,
+    QgsLayerTreeLayer,
+    QgsPointXY,
+    QgsProject,
+    QgsVectorLayer,
+)
 
 from ..definitions.constants import Profile, Unit
+from ..qgis_plugin_tools.tools.exceptions import QgsPluginNetworkException
+from ..qgis_plugin_tools.tools.network import fetch
+from ..qgis_plugin_tools.tools.resources import plugin_name
+
+LOGGER = logging.getLogger(plugin_name())
 
 
 @dataclass
@@ -25,7 +39,59 @@ class IsochroneOpts:
 class IsochroneCreator:
     def __init__(self, opts: IsochroneOpts) -> None:
         self.opts = opts
+        self.base_url = self.opts.url
+        if not self.base_url[-1] == "/":
+            self.base_url += "/"
+        self.base_url += "isochrone"
+        self.params = {
+            "profile": self.opts.profile.value,
+            "buckets": 4,
+        }
+        if self.opts.unit == Unit.METERS:
+            self.params["distance_limit"] = self.opts.distance
+            self.params["time_limit"] = -1
+        else:
+            self.params["time_limit"] = 60 * self.opts.distance
 
     def create_isochrone_layer(self) -> QgsVectorLayer:
         """Creates a polygon QgsVectorLayer containing isochrones for points"""
-        raise NotImplementedError
+        layer_name = (
+            f"{self.opts.distance} {self.opts.unit.value} by {self.opts.profile.value}"
+        )
+        isochrone_layer = QgsVectorLayer(
+            "Polygon?crs=epsg:4326&index=yes", layer_name, "memory"
+        )
+        isochrone_layer.renderer().symbol().setOpacity(0.25)
+        for idx, point in enumerate(self.opts.layer.getFeatures()):
+            point = point.geometry().asPoint()
+            isochrone_params = self.params
+            isochrone_params["point"] = f"{point.y()},{point.x()}"
+            try:
+                isochrone_json = fetch(self.base_url, params=isochrone_params)
+            except QgsPluginNetworkException as e:
+                LOGGER.warn(f"Request failed for point {point}: {e}")
+            bucketed_isochrones = json.loads(isochrone_json)["polygons"]
+            for polygon_in_bucket in bucketed_isochrones:
+                feature = QgsFeature()
+                feature.setGeometry(
+                    QgsGeometry.fromPolygonXY(
+                        [
+                            [
+                                QgsPointXY(pt[0], pt[1])
+                                for pt in polygon_in_bucket["geometry"]["coordinates"][
+                                    0
+                                ]
+                            ]
+                        ]
+                    )
+                )
+                isochrone_layer.dataProvider().addFeature(feature)
+            # update layer's extent when new features have been added
+            if idx and idx % 10 == 0:
+                LOGGER.info(
+                    f"{idx} out of {self.opts.layer.featureCount()} objects fetched"
+                )
+        isochrone_layer.updateExtents()
+        QgsProject.instance().addMapLayer(isochrone_layer, False)
+        root = QgsProject.instance().layerTreeRoot()
+        root.insertChildNode(1, QgsLayerTreeLayer(isochrone_layer))
