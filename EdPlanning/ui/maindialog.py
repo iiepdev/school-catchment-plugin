e3f18530525988952f4693f6b2398c6642156593
Riku Oja <riku.oja@kapsi.fi>
2021-05-25 16:36:38 +0300

    More sensible estimate; warn the user of a heavy operation

--- EdPlanning/ui/maindialog.py
+++ EdPlanning/ui/maindialog.py
@@ -1,4 +1,4 @@
-from math import ceil, log
+from math import ceil, pow
 from typing import Optional
 
 from PyQt5.QtCore import pyqtSlot
@@ -7,10 +7,15 @@ from qgis.core import QgsMapLayerProxyModel
 
 from ..core.isochrone_creator import IsochroneOpts
 from ..definitions.constants import Profile, Unit
+from ..qgis_plugin_tools.tools.exceptions import QgsPluginException
 from ..qgis_plugin_tools.tools.resources import load_ui
 from ..qgis_plugin_tools.tools.settings import get_setting
 
 FORM_CLASS = load_ui("main.ui")
+
+
+class TooHeavyOperationException(QgsPluginException):
+    pass
 
 
 class MainDialog(QDialog, FORM_CLASS):  # type: ignore
@@ -46,7 +51,12 @@ class MainDialog(QDialog, FORM_CLASS):  # type: ignore
     def _get_duration(self) -> Optional[int]:
         """
         Estimated duration of the calculation in minutes,
-        assuming logarithmic scaling
+        assuming O(2^(5K-1) - 2^(4K-4)) scaling.
+
+        Here K is one step in the graph, and we assume the graph branches
+        once per minute of walking. This is a realistic magnitude
+        in case of neigborhoods made for walking. Simplifies to
+        O(2^(5K)-2^(4K))~=O(2^(5K)) for large K.
         """
         opts = self.read_isochrone_options()
         if opts.check_if_opts_set():
@@ -55,20 +65,24 @@ class MainDialog(QDialog, FORM_CLASS):  # type: ignore
             if opts.unit == Unit.METERS:
                 # assuming walking speed 5 km/h = 83.3 m/min
                 distance_in_minutes_by_foot = opts.distance / 83.3  # type: ignore
-            if opts.profile == Profile.CYCLING:
-                # assuming biking speed 15 km/h
+            elif opts.profile == Profile.CYCLING:
+                # assuming biking speed 25 km/h
                 distance_in_minutes_by_foot = 3 * distance_in_minutes_by_foot  # type: ignore  # noqa
             elif opts.profile == Profile.DRIVING:
                 # assuming driving speed 50 km/h
                 distance_in_minutes_by_foot = 10 * distance_in_minutes_by_foot  # type: ignore  # noqa
             if count:
-                # 30 minute distance takes ~ 1 minute for 1000 points
-                return int(
-                    ceil(
-                        (float(count) / 1000)
-                        * (log(distance_in_minutes_by_foot / 30, 2) + 1)
-                    )
+                # Normalize, knowing that 60 minute distance by car
+                # (equivalent of 600 minutes by foot) takes ~ 10 minute
+                # to calculate for 1000 points.
+                minutes_per_thousand_points = 10 * pow(
+                    2, 5 * distance_in_minutes_by_foot - 3000
                 )
+                # Network calls ~ 1 minute for 1000 pts.
+                total = float(count / 1000) * float(minutes_per_thousand_points + 1)
+                if total > 120:
+                    raise TooHeavyOperationException()
+                return ceil(total)
         return None
 
     @staticmethod
@@ -115,7 +129,7 @@ class MainDialog(QDialog, FORM_CLASS):  # type: ignore
         elif selected_unit == Unit.METERS:
             step = 500
             min_ = 500
-            max_ = 5000
+            max_ = 10000
             default = 2000
 
         self.spinbox_distance.setSingleStep(step)
@@ -128,8 +142,18 @@ class MainDialog(QDialog, FORM_CLASS):  # type: ignore
         """Updates estimated duration based on currently
         selected isochrone options"""
 
-        duration = self._get_duration()
-        if duration is not None:
-            self.duration_label.setText(f"Estimated time to calculate: {duration} mins")
-        else:
-            self.duration_label.setText("")
+        try:
+            duration = self._get_duration()
+            if duration is not None:
+                self.duration_label.setText(
+                    f"Estimated processing time: {duration} mins"
+                )
+            else:
+                self.duration_label.setText("")
+            self.duration_label.setStyleSheet("color: black")
+        except (OverflowError, TooHeavyOperationException):
+            self.duration_label.setText(
+                "Too many points or too large distance selected.\nRunning with these "
+                "settings may take several hours or days."
+            )
+            self.duration_label.setStyleSheet("color: red")
