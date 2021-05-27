b5ce4b47ba84bfed1d9a76798afecf113cba3d45
Riku Oja <Rikuoja@users.noreply.github.com>
2021-05-27 10:43:36 +0300

    Move interface to multiple panels (#12)
    * Move interface to multiple panels
    
    * Fix typing errors
    
    * Fix flake8 type annotations
    
    * Add missing qgsspinbox widget

---
+++ EdPlanning/ui/catchment_area_panel.py
@@ -0,0 +1,140 @@
+import logging
+from math import floor, pow
+from typing import Optional
+
+from PyQt5.QtWidgets import QDialog
+from qgis.core import QgsMapLayerProxyModel
+
+from ..definitions.constants import Profile, Unit
+from ..definitions.gui import Panels
+from ..qgis_plugin_tools.tools.exceptions import QgsPluginException
+from ..qgis_plugin_tools.tools.resources import plugin_name
+from .base_panel import BasePanel
+
+LOGGER = logging.getLogger(plugin_name())
+
+
+class TooHeavyOperationException(QgsPluginException):
+    pass
+
+
+class CatchmentAreaPanel(BasePanel):
+    def __init__(self, dialog: QDialog) -> None:
+        super().__init__(dialog)
+        self.panel = Panels.CatchmentAreas
+
+    def setup_panel(self) -> None:
+        self.dlg.combobox_layer.setFilters(QgsMapLayerProxyModel.PointLayer)
+        self.__update_duration_label()
+
+        # connect the signals, since pyqt slot decorator cannot be used
+        self.dlg.radiobtn_mins.clicked.connect(self.on_radiobtn_mins_clicked)
+        self.dlg.radiobtn_meters.clicked.connect(self.on_radiobtn_meters_clicked)
+        self.dlg.radiobtn_walking.clicked.connect(self.on_radiobtn_walking_clicked)
+        self.dlg.radiobtn_cycling.clicked.connect(self.on_radiobtn_cycling_clicked)
+        self.dlg.radiobtn_driving.clicked.connect(self.on_radiobtn_driving_clicked)
+        self.dlg.combobox_layer.layerChanged.connect(
+            self.on_combobox_layer_layerChanged
+        )
+        self.dlg.spinbox_distance.valueChanged.connect(
+            self.on_spinbox_distance_valueChanged
+        )
+
+    def _get_duration(self) -> Optional[int]:
+        """
+        Estimated duration of the calculation in minutes,
+        assuming O(2^(5K-1) - 2^(4K-4)) scaling.
+
+        Here K is one step in the graph. Simplifies to
+        O(2^(5K)-2^(4K))~=O(2^(5K)) for large K.
+        """
+        opts = self.dlg.read_isochrone_options()
+        if opts.check_if_opts_set():
+            count = opts.layer.featureCount()  # type: ignore
+            distance_in_minutes_by_foot: int = opts.distance  # type: ignore
+            if opts.unit == Unit.METERS:
+                # assuming walking speed 5 km/h = 83.3 m/min
+                distance_in_minutes_by_foot = opts.distance / 83.3  # type: ignore
+            elif opts.profile == Profile.CYCLING:
+                # assuming biking speed 25 km/h
+                distance_in_minutes_by_foot = 3 * distance_in_minutes_by_foot  # type: ignore  # noqa
+            elif opts.profile == Profile.DRIVING:
+                # assuming driving speed 50 km/h
+                distance_in_minutes_by_foot = 10 * distance_in_minutes_by_foot  # type: ignore  # noqa
+            if count:
+                # Normalize, knowing that 60 minute distance by car
+                # (equivalent of 600 minutes by foot) takes ~ 10 minute
+                # to calculate for 1000 points.
+                # No idea why K should be so small here.
+                minutes_per_thousand_points = 10 * pow(
+                    2, distance_in_minutes_by_foot / 100 - 6
+                )
+                # Network calls ~ 1 minute for 1000 pts.
+                total = float(count / 1000) * float(minutes_per_thousand_points + 1)
+                if total > 120:
+                    raise TooHeavyOperationException()
+                return floor(total)
+        return None
+
+    def on_radiobtn_mins_clicked(self) -> None:
+        self.__update_unit_selector(Unit.MINUTES)
+        self.__update_duration_label()
+
+    def on_radiobtn_meters_clicked(self) -> None:
+        self.__update_unit_selector(Unit.METERS)
+        self.__update_duration_label()
+
+    def on_radiobtn_walking_clicked(self) -> None:
+        self.__update_duration_label()
+
+    def on_radiobtn_cycling_clicked(self) -> None:
+        self.__update_duration_label()
+
+    def on_radiobtn_driving_clicked(self) -> None:
+        self.__update_duration_label()
+
+    def on_combobox_layer_layerChanged(self) -> None:  # noqa
+        self.__update_duration_label()
+
+    def on_spinbox_distance_valueChanged(self) -> None:  # noqa
+        self.__update_duration_label()
+
+    def __update_unit_selector(self, selected_unit: Unit) -> None:
+        """Sets unit spinbox min, max, and step values
+        based on currently selected unit"""
+        if selected_unit == Unit.MINUTES:
+            step = 5
+            min_ = 5
+            max_ = 120
+            default = 30
+        elif selected_unit == Unit.METERS:
+            step = 500
+            min_ = 500
+            max_ = 10000
+            default = 2000
+
+        self.dlg.spinbox_distance.setSingleStep(step)
+        self.dlg.spinbox_distance.setMinimum(min_)
+        self.dlg.spinbox_distance.setMaximum(max_)
+        self.dlg.spinbox_distance.setValue(default)
+        self.dlg.spinbox_distance.setClearValue(default)
+
+    def __update_duration_label(self) -> None:
+        """Updates estimated duration based on currently
+        selected isochrone options"""
+
+        try:
+            duration = self._get_duration()
+            if duration is not None:
+                self.dlg.duration_label.setText(
+                    f"Estimated processing time: {duration} mins"
+                )
+            else:
+                self.dlg.duration_label.setText("")
+            self.dlg.duration_label.setStyleSheet("color: black")
+        except (OverflowError, TooHeavyOperationException):
+            self.dlg.duration_label.setText(
+                "Too many points or too large distance selected.\nRunning with these "
+                "settings may take several hours or days."
+            )
+            self.dlg.duration_label.setStyleSheet("color: red")
---
+++ EdPlanning/ui/settings_panel.py
@@ -0,0 +1,49 @@
+import logging
+import webbrowser
+
+from PyQt5.QtWidgets import QDialog
+
+from ..definitions.gui import Panels
+from ..qgis_plugin_tools.tools.custom_logging import (
+    LogTarget,
+    get_log_level_key,
+    get_log_level_name,
+)
+from ..qgis_plugin_tools.tools.resources import plugin_name, plugin_path
+from ..qgis_plugin_tools.tools.settings import set_setting
+from .base_panel import BasePanel
+
+LOGGER = logging.getLogger(plugin_name())
+
+LOGGING_LEVELS = ["DEBUG", "INFO", "WARNING", "ERROR", "CRITICAL"]
+
+
+class SettingsPanel(BasePanel):
+    def __init__(self, dialog: QDialog) -> None:
+        super().__init__(dialog)
+        self.panel = Panels.Settings
+
+    def setup_panel(self) -> None:
+        self.dlg.combo_box_log_level_file.clear()
+        self.dlg.combo_box_log_level_console.clear()
+
+        self.dlg.combo_box_log_level_file.addItems(LOGGING_LEVELS)
+        self.dlg.combo_box_log_level_console.addItems(LOGGING_LEVELS)
+        self.dlg.combo_box_log_level_file.setCurrentText(
+            get_log_level_name(LogTarget.FILE)
+        )
+        self.dlg.combo_box_log_level_console.setCurrentText(
+            get_log_level_name(LogTarget.STREAM)
+        )
+
+        self.dlg.combo_box_log_level_file.currentTextChanged.connect(
+            lambda level: set_setting(get_log_level_key(LogTarget.FILE), level)
+        )
+
+        self.dlg.combo_box_log_level_console.currentTextChanged.connect(
+            lambda level: set_setting(get_log_level_key(LogTarget.STREAM), level)
+        )
+
+        self.dlg.btn_open_log.clicked.connect(
+            lambda _: webbrowser.open(plugin_path("logs", f"{plugin_name()}.log"))
+        )
